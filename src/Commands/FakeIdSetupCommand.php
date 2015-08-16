c4e95fbd9dc323fb7b292f8303ce6c449670e20b
Propaganistas <propaganistas@gmail.com>
2015-08-16 15:59:31 +0200

    Drop phpseclib for own implementation

--- src/Commands/FakeIdSetupCommand.php
+++ src/Commands/FakeIdSetupCommand.php
@@ -31,15 +31,11 @@ class FakeIdSetupCommand extends Command
     public function fire()
     {
         // Get a pseudo-random prime.
-        $min = new BigInteger(1e7);
-        $max = new BigInteger(Optimus::MAX_INT);
-        $prime = $max->randomPrime($min, $max);
+        $prime = gmp_intval(gmp_nextprime(mt_rand(1e7, Optimus::MAX_INT)));
 
         // Calculate the inverse.
-        $a = new BigInteger($prime);
-        $b = new BigInteger(Optimus::MAX_INT + 1);
-        if (!$inverse = $a->modInverse($b)) {
-            $this->error("Error during calculation of FakeId settings. Please rerun this command.");
+        if (!$inverse = gmp_intval(gmp_invert($prime, (Optimus::MAX_INT + 1)))) {
+            $this->error("Error during calculation of FakeId settings. Please re-run this command (php artisan fakeid:setup).");
             return;
         }
 
@@ -49,22 +45,25 @@ class FakeIdSetupCommand extends Command
         // Write in environment file.
         $path = base_path('.env');
 
-        if (file_exists($path)) {
-            // Remove existing configuration.
-            $fileArray = file($path);
-            foreach($fileArray as $k => $line) {
-                if (strpos($line, 'FAKEID_') === 0) {
-                    unset($fileArray[$k]);
-                }
+	    if (!file_exists($path)) {
+		    $this->error("Environment file (.env) not found. Aborting FakeId setup!");
+		    return;
+	    }
+
+        // Remove existing configuration.
+        $fileArray = file($path);
+        foreach($fileArray as $k => $line) {
+            if (strpos($line, 'FAKEID_') === 0) {
+                unset($fileArray[$k]);
             }
-
-            // Append new configuration.
-            $fileArray[] = "\nFAKEID_PRIME=" . $prime;
-            $fileArray[] = "\nFAKEID_INVERSE=" . $inverse;
-            $fileArray[] = "\nFAKEID_RANDOM=" . $rand;
-            file_put_contents($path, implode('', $fileArray), LOCK_EX);
-
-            $this->info("FakeId configured correctly.");
         }
+
+        // Append new configuration.
+        $fileArray[] = "\nFAKEID_PRIME=" . $prime;
+        $fileArray[] = "\nFAKEID_INVERSE=" . $inverse;
+        $fileArray[] = "\nFAKEID_RANDOM=" . $rand;
+        file_put_contents($path, implode('', $fileArray), LOCK_EX);
+
+        $this->info("FakeId configured correctly.");
     }
 }
