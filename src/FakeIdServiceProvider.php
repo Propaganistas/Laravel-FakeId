4840976b94b682f4b47509bc62df7fd878ce0ee4
Tarek Adam <tarek.adam@gmail.com>
2017-01-07 06:27:20 -0800

    replaces class_uses() trait detection with laravel's helper class_uses_recursive()

--- src/FakeIdServiceProvider.php
+++ src/FakeIdServiceProvider.php
@@ -67,7 +67,7 @@ class FakeIdServiceProvider extends ServiceProvider
                 $instance = $this->container->make($class);
 
                 // Decode FakeId first if applicable.
-                if (in_array('Propaganistas\LaravelFakeId\FakeIdTrait', self::class_uses_deep($class))) {
+                if (in_array('Propaganistas\LaravelFakeId\FakeIdTrait', class_uses_recursive($class))) {
                     $value = $this->container->make('fakeid')->decode($value);
                 }
 
@@ -86,29 +86,5 @@ class FakeIdServiceProvider extends ServiceProvider
             });
         });
     }
-
-	private static function class_uses_deep($class, $autoload = true)
-	{
-		$traits = [];
-
-		// Get traits of all parent classes
-		do {
-			$traits = array_merge(class_uses($class, $autoload), $traits);
-		} while ($class = get_parent_class($class));
-
-		// Get traits of all parent traits
-		$traitsToSearch = $traits;
-		while (!empty($traitsToSearch)) {
-			$newTraits = class_uses(array_pop($traitsToSearch), $autoload);
-			$traits = array_merge($newTraits, $traits);
-			$traitsToSearch = array_merge($newTraits, $traitsToSearch);
-		};
-
-		foreach ($traits as $trait => $same) {
-			$traits = array_merge(class_uses($trait, $autoload), $traits);
-		}
-
-		return array_unique($traits);
-	}
 
 }
